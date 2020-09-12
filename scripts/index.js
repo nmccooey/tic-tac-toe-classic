bbe8527c66d0d8c7ca50054f7446815b130dffc6
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-09-12 19:34:55 -0400

    Added background sound and changed colors in UI.

--- scripts/index.js
+++ scripts/index.js
@@ -8,8 +8,8 @@ function launchGameMenu() {
     dismissible: false
   };
 
-  let gameMenuinstance = M.Modal.init(modalElement, options);
-  gameMenuinstance.open();
+  let gameMenuInstance = M.Modal.init(modalElement, options);
+  gameMenuInstance.open();
   let gameMode = "hvh";
 
   document.querySelector("#next-button").addEventListener("click", function() {
@@ -24,7 +24,7 @@ function launchGameMenu() {
 
   // Modal for player to choose character.
   function launchChooseCharacterModal() {
-    gameMenuinstance.close();
+    gameMenuInstance.close();
     let modalElement = document.querySelector("#choose-character-modal");
     let options = {
       inDuration: 0,
@@ -93,25 +93,105 @@ function startGame(gameMode, characterName) {
   // Human
   let playerOne = {
     name: `<i class="fas fa-${characterName}"></i>`,
+    characterName: characterName,
     score: 0
   }
   // Computer
   let playerTwo = {
-    name: "Goblin",
+    name: `<i class="fab fa-optin-monster"></i>`,
+    characterName: "Monster",
     score: 0
   }
+  let board = ["","","","","","","","",""];
+  let currentPlay = playerOne;
 
+  startBackgroundMusic();
   initEventListeners();
+
+  // Starts the background music on a loop.
+  function startBackgroundMusic() {
+    let backgroundMusic = new Audio("./sounds/scary-background.mp3");
+    backgroundMusic.play();
+    backgroundMusic.volume = 0.3;
+    backgroundMusic.loop = true;
+  }
 
   // Initializes event listeners for each game spot.
   function initEventListeners() {
     document.querySelectorAll(".game-spot").forEach(gameSpot => {
       gameSpot.addEventListener("click", event => {
-          if (gameSpot.innerHTML == "") {
-            gameSpot.innerHTML = playerOne.name;
-            // Check winner.
-          }
+        if (gameSpot.innerHTML == "") {
+          gameSpot.innerHTML = currentPlay.name;
+          board[gameSpot.getAttribute("data-index")] = currentPlay;
+          checkWinner(board, currentPlay);
+          changePlayer();
+        }
       });
     });
+  }
+
+  function changePlayer() {
+    if (currentPlay == playerOne) {
+      currentPlay = playerTwo;
+    } else {
+      currentPlay = playerOne;
+    }
+  }
+
+  function checkWinner(board, currentPlay) {
+    let winner = winTest(board, currentPlay);
+    if (winner == playerOne || winner == playerTwo) {
+      setTimeout(function() {
+          alert(`The winner is ${winner.characterName}`);
+          location.reload(true);
+      }, 2)
+    } else if (winner == "tie") {
+      setTimeout(function() {
+          alert(`It's a Tie!`);
+          location.reload(true);
+      }, 2)
+    }
+  }
+
+  function winTest(board, side) {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8]
+    ];
+
+    for (let i = 0; i < winConditions.length; i++) { 
+      let sum = 0;
+      let w = winConditions[i];
+      
+      for (let b = 0; b < w.length; b++) {
+        if(board[w[b]] === side) {
+            sum++
+        }
+      }
+      
+      if (sum === 3) {
+        return side;
+      }
+
+      // check tie:
+      let spotsTaken = 0;
+      for (let i = 0; i < board.length; i++) {
+        if(board[i] !== "") {
+            spotsTaken++;
+        }
+      }
+
+      if (spotsTaken == 9) {
+        return "tie";
+      } 
+    }
+
+    return false;
   }
 }
